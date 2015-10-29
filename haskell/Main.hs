d346efd6df5719d24d696955a089ec8387d98aef
David Fox <dsf@seereason.com>
2015-10-29 09:08:17 -0700

    Add README.md, second example formula

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -12,8 +12,9 @@ import Types
 import Failing
 import Equality
 
-main = putStrLn . show . meson . equalitize $ wishnu
+main = (putStrLn . show . meson . equalitize) wishnu {- ewd -}
 
+-- ∃x. (x=f[g[x]] ∧ ∀x'. (x'=f[g[x']] ⇒ x=x')) ⇔ ∃y. (y=g[f[y]] ∧ ∀y'. (y'=g[f[y']] ⇒ y=y'))
 wishnu :: Formula FOL
 wishnu = (Iff (Exists "x" (And (Atom (R "=" [Var "x", (Fn "f" [Fn "g" [Var "x"]])]))
                                (Forall "x'" (Imp (Atom (R "=" [Var "x'", (Fn "f" [Fn "g" [Var "x'"]])]))
@@ -21,3 +22,14 @@ wishnu = (Iff (Exists "x" (And (Atom (R "=" [Var "x", (Fn "f" [Fn "g" [Var "x"]]
               (Exists "y" (And (Atom (R "=" [Var "y", (Fn "g" [Fn "f" [Var "y"]])]))
                                (Forall "y'" (Imp (Atom (R "=" [Var "y'", (Fn "g" [Fn "f" [Var "y'"]])]))
                                                  (Atom (R "=" [Var "y", Var "y'"])))))))
+
+-- (∀x. (f[x] ⇒ g[x])) ∧
+-- (∃x. f[x]) ∧
+-- (∀x. (∀y. (g[x] ∧ g[y] ⇒ x .=. y))) ⇒
+-- (∀y. (g[y] ⇒ f[y]))
+ewd :: Formula FOL
+ewd = Imp (And (And (Forall "x" (Imp (Atom (R "f" [Var "x"])) (Atom (R "g" [Var "x"]))))
+                    (Exists "x" (Atom (R "f" [Var "x"]))))
+               (Forall "x" (Forall "y" (Imp (And (Atom (R "g" [Var "x"])) (Atom (R "g" [Var "y"])))
+                                            (Atom (R "=" [Var "x", Var "y"]))))))
+          (Forall "y" (Imp (Atom (R "g" [Var "y"])) (Atom (R "f" [Var "y"]))))
